98acec7eb761335b76208e998ca6cd575ef8f913
Maulidani Mahmud <maulidanimahmud@gmail.com>
2023-01-09 11:51:39 +0800

    update

--- app/src/main/java/go/kejaksaannegeriluwutimur/view/kepaladesa/layananperdatadantatausahanegara/LayananPerdataDanTataUsahaNegaraActivity.kt
+++ app/src/main/java/go/kejaksaannegeriluwutimur/view/kepaladesa/layananperdatadantatausahanegara/LayananPerdataDanTataUsahaNegaraActivity.kt
@@ -1,12 +1,53 @@
 package go.kejaksaannegeriluwutimur.view.kepaladesa.layananperdatadantatausahanegara
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import go.kejaksaannegeriluwutimur.R
 
 class LayananPerdataDanTataUsahaNegaraActivity : AppCompatActivity() {
+    private val imgBack: ImageView by lazy { findViewById(R.id.iv_back) }
+    private val cardLayananPermohonanMoU: CardView by lazy { findViewById(R.id.cv_item_layanan_permohonan_mou) }
+    private val cardPelayananBantuanHukum: CardView by lazy { findViewById(R.id.cv_item_pelayanan_bantuan_hukum) }
+    private val cardPelayananPertimbanganHukum: CardView by lazy { findViewById(R.id.cv_item_pelayanan_pertimbangan_hukum) }
+    private val cardPelayananHukumGratis: CardView by lazy { findViewById(R.id.cv_item_pelayanan_hukum_gratis) }
+    private val cardTindakanHukumLain: CardView by lazy { findViewById(R.id.cv_item_tindakan_hukum_lain) }
+    private val cardChat: CardView by lazy { findViewById(R.id.cv_chat) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layanan_perdata_dan_tata_usaha_negara)
+
+        setOnClick()
+
+    }
+
+    private fun setOnClick() {
+
+        imgBack.setOnClickListener { finish() }
+
+        cardLayananPermohonanMoU.setOnClickListener {
+            startActivity(Intent(applicationContext, PermohonanMouActivity::class.java))
+        }
+        cardPelayananBantuanHukum.setOnClickListener {
+            startActivity(Intent(applicationContext, BantuanHukumActivity::class.java))
+        }
+        cardPelayananPertimbanganHukum.setOnClickListener {
+            startActivity(Intent(applicationContext, PertimbanganHukumActivity::class.java))
+        }
+        cardPelayananHukumGratis.setOnClickListener {
+            startActivity(Intent(applicationContext, HukumGratisActivity::class.java))
+        }
+        cardTindakanHukumLain.setOnClickListener {
+            startActivity(Intent(applicationContext, HukumLainActivity::class.java))
+        }
+
+        cardChat.setOnClickListener {
+            //
+        }
+
+
     }
 }
